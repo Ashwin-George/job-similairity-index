44d79f33abab04112313b8198c01a04a4a8be785
Ashwin George <85630977+Ashwin-George@users.noreply.github.com>
2023-11-17 00:16:26 +0530

    first commit

---
+++ bert-similarity.py
@@ -0,0 +1,52 @@
+import constants
+
+import sentence_transformers
+from sentence_transformers import util
+
+model = sentence_transformers.SentenceTransformer('paraphrase-MiniLM-L6-v2')
+# Example paragraphs
+paragraph1 = constants.para1
+paragraph2 = constants.para3
+
+
+# Lowercasing
+paragraph1 = paragraph1.lower()
+paragraph2 = paragraph2.lower()
+# print(paragraph1)
+
+# Get paragraph embeddings
+embeddings = model.encode([paragraph1, paragraph2])
+
+# Calculate cosine similarity
+similarity = sentence_transformers.util.pytorch_cos_sim(embeddings[0], embeddings[1])
+print(similarity.item())
+
+def preprocess_text(text):
+    # Tokenize the text
+    tokens = word_tokenize(text)
+
+    # Convert to lowercase
+    tokens = [token.lower() for token in tokens]
+
+    tagged_tokens = pos_tag(tokens)
+    tokens = [word for word, pos in tagged_tokens if
+              not (pos.startswith('VB') or pos.startswith('CONJ') or pos.startswith('ADP') or pos.startswith('ADV'))]
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [token for token in tokens if token not in stop_words]
+
+    print("\n length", len(tokens))
+
+    # Lemmatization using WordNet Lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+
+    # Remove non-alphabetic characters
+    # tokens = [token for token in tokens if token.isalpha()]
+
+    preprocessed_text = ' '.join(tokens)
+
+    sentences = sent_tokenize(preprocessed_text)
+    print("\nsentences", sentences)
+    print("\npreprocessed_text", preprocessed_text)
+    return preprocessed_text
---
+++ constants.py
@@ -0,0 +1,65 @@
+
+para1= '''
+Feb’16 – Present with Tata Consultancy Services, Gurugram as Data Scientist As Data Scientist (TCS, Gurugram) – Jan’20 -
+Present Client: Belgium Post (Logistics) Technology Stack: AWS Cloud Platform (Athena, SageMaker and S3), Python and R,
+Machine Learning Algorithms, Power BI, Agile Methodology Role: • Executing data acquisition & extraction, data analysis
+(quantitative and qualitative), data presentation and reporting; proficient in analysing & presenting conclusions gained
+from analyzing data • Implementing end-to-end time-series forecasting solutions on AWS/ building python package and
+deploying a flask API using docker • Developing business solutions by using the available data and applying statistical
+& learning methods • Utilizing predictive modelling to increase & optimize customer experiences and revenue generation •
+Analysing the user needs and requirements for determining feasibility of design within time and cost constraints
+Highlights: • Developed a forecasting model to predict volume of parcel deliveries at sorting & distribution centers
+using AWS DeepAR & Facebook Prophet to maximize revenue and target higher volume delivery annually • Deployed a model
+for feature extraction from AWS S3 using Athena and identified relevant features on historical data by analyzing lag
+variables, moving averages, ACF/PACF plots and correlation matrices As Senior Software Engineer (TCS, Gurugram) – Jan’18
+– Dec’19 Client: Lloyd’s Banking Group (BFSI) Technology Stack: Java, Rest Web Services, Spring MVC, SQL, Jenkins,
+Selenium, Postman, Agile Methodology Highlight: • Developed an end-to-end Foreign Account Tax Compliance Act (FATCA)
+form which contained total 16 Java API services, created sorted procedures, provided support for system integration
+testing, approval from governance forum, LUAT subscription and go-live production As System Engineer (TCS, Pune) –
+Feb’16 – Dec’17 Client: Lloyd’s Banking Group (BFSI) Technology Stack: Angular, Bootstrap, CSS3, HTML, Visual Code, D3
+Charts, Bitbucket, Bamboo, Karma, Cucumber, Agile Methodology Highlights: • Created UI services using Angular based on
+customer requirements • Designed and created different dynamic pages and applied D3 charts for visualization of annual
+growth of services like non-profit, recordkeeping, retirement plan, pension plan for the client
+
+'''
+
+para2='''
+PocketOps is a cutting-edge technology company dedicated to revolutionizing the way businesses operate in the digital
+landscape. Our team is passionate about creating innovative solutions that empower organizations to thrive in the
+fast-paced world of technology. As we continue to grow, we are seeking a talented and experienced DevOps Engineer to
+join our dynamic team. As a DevOps Engineer at PocketOps, you will play a crucial role in enhancing our infrastructure
+and streamlining our development and deployment processes. You will work collaboratively with cross- functional teams to
+ensure the reliability, scalability, and security of our systems. If you are a self- motivated individual with a strong
+technical background and a passion for automation, we invite you to apply and contribute to the success of our
+innovative projects. - Design, implement, and maintain automation processes for infrastructure as code (IaC) using tools
+such as Terraform and Ansible. - Collaborate with development and operations teams to integrate and streamline the build
+and deployment pipelines. - Monitor and optimize system performance, ensuring high availability, scalability, and
+security of production environments. - Implement and manage CI/CD pipelines to facilitate efficient and reliable
+software delivery. - Troubleshoot and resolve issues related to infrastructure, deployments, and general system
+reliability. - Stay abreast of industry best practices and emerging technologies to drive continuous improvement.
+Required Technical Skills: - Proficient in scripting languages such as Python, Bash, or Ruby. - Hands-on experience with
+containerization technologies, preferably Docker and Kubernetes. - Strong understanding of cloud computing platforms,
+such as AWS, Azure, or GCP. - Expertise in configuration management tools like Ansible, Puppet, or Chef. - Familiarity
+with version control systems (Git) and collaborative coding practices. - Knowledge of monitoring and logging tools such
+as Prometheus, Grafana, ELK stack, or Splunk. - Experience with CI/CD tools like Jenkins, GitLab CI, or Travis CI. -
+Solid understanding of networking principles and security best practices. Qualifications: How to Apply: - Bachelor's
+degree in Computer Science, Information Technology, or a related field. - Minimum of 1 year of experience in a DevOps or
+related role. - Proven track record of successfully implementing and maintaining DevOps practices. - Strong analytical
+and problem-solving skills. - Excellent communication and collaboration skills. If you are excited about the prospect of
+joining a dynamic team at the forefront of technology, please submit your resume and a cover letter highlighting your
+relevant experience to [email@example.com]. Please include "DevOps Engineer Application - [Your Name]" in the email
+subject line. PocketOps is an equal opportunity employer and values diversity in the workplace. We encourage
+applications from individuals of all backgrounds and experiences.
+'''
+
+
+para3='''
+Collect, clean, and prepare data for analysis  Develop and implement data science methodologies and algorithms 
+Build and evaluate predictive models  Communicate findings and insights to stakeholders  Collaborate with other
+members of the team to develop and implement data-driven solutions Qualifications Bonus Qualifications  Master's degree
+in Data Science, Computer Science, Statistics, or a related field  3+ years of experience in a data science role 
+Strong programming skills in Python and/or R  Experience with machine learning algorithms and statistical methods 
+Excellent data visualization and communication skills  Experience with cloud computing platforms such as AWS or Azure 
+Experience with big data technologies such as Hadoop and Spark  Experience with natural language processing and
+computer vision  Experience with DevOps and CI/CD practices To Apply Please submit your resume and a cover letter to
+jobs@[Company Name].com.'''
